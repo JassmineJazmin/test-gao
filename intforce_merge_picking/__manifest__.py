e13acb2c9059826dabd0691ba1b4d041d36c1363
JassmineJazmin <58958422+JassmineJazmin@users.noreply.github.com>
2021-08-19 14:00:02 -0500

    Add files via upload

---
+++ intforce_merge_picking/__manifest__.py
@@ -0,0 +1,27 @@
+
+{
+    'name': "Merge Multiple Delivery Orders / Incoming Shipments",
+    'summary': """This module will support you to merge multiple delivery orders / incoming shipments in single picking""",
+    'description': """
+    Merge Any Type of Picking 
+    Merge Incoming Shipment
+    Merge Delivery Orders
+    """,
+    'author': "Intforce Software Private Limited",
+    'website': "https://intforce.co.in",
+    'category': 'Uncategorized',
+    'version': '13.0.1',
+    "license" : "Other proprietary",
+    'images': ['static/description/banner.png'],
+    "price": "28.00",
+    "currency": "EUR",
+    'category': 'Uncategorized',
+
+    'depends': ['base','stock'],
+
+    'data': [
+        'views/merge_picking.xml'
+       
+    ],
+  
+}
